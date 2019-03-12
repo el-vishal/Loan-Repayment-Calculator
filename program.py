5fffa2f9035f37acf6fc91423c7c11527fcb2ae8
Billa:D <el_vishal@hotmail.com>
2019-03-12 22:31:12 +0000

    Final update
    Fixed bugs
    Added input validation
    Remove redundant codes
    Added Icon
    Added comments
    Multiple exe file versions

--- program.py
+++ program.py
@@ -1,9 +1,8 @@
 #Importing GUI library
 import PySimpleGUI as sg
 
-
 #GUI Decoration
-layout = [[sg.Text('Monthly Loan Repayment Calculator', size=(30, 1), font=("Arial", 25), justification='center', text_color='black')], #title
+layout = [[sg.Text('Equal Monthly Loan Repayment Calculator', size=(40, 1), font=("Arial", 20), justification='center', text_color='black')], #title
           #[sg.Text('Vishal Sharma - vs200 / 189034637', font=("Arial", 8), )], #sub-title
           [sg.Text('Loan Amount', size=(18,1), justification='left'), sg.InputText('')], #Loan amount + textbox
           [sg.Text('Interest Rate (decimal)', size=(17,1), justification='left'), sg.InputText('', size=(10,1)), #Interest Rate + textbox
@@ -12,41 +11,66 @@ layout = [[sg.Text('Monthly Loan Repayment Calculator', size=(30, 1), font=("Ari
           [sg.Text('_' * 100, size=(70, 1))], #line divider
           [sg.Submit(button_color=('white', 'green')), sg.Cancel(button_color=('white', 'firebrick3'))]] #buttons
 
+# function to output table
+def table_function(a):
+    header_list = ['Payment No', 'Payment Amount', 'Principal Paid', 'Interest Paid', 'Loan balance']  # headers
+    data_1 = a  #data is passed as parameter
+
+    # table decoration
+    sg.SetOptions(element_padding=(0, 0))
+    layout2 = [[sg.Table(values=data_1,
+                         headings=header_list,
+                         max_col_width=25,
+                         auto_size_columns=True,
+                         justification='center',
+                         alternating_row_color='lightblue',
+                         num_rows=min(len(data_1), 20))],
+               [sg.Cancel()]]
+    # show output
+    table_window = sg.Window('Table', grab_anywhere=False).Layout(layout2)
+    event = table_window.Read()
+    table_window.Close() #Close on Exit
+
+
+
+
+#Create a window
+main_window = sg.Window('Loan Repayment Calculator', auto_size_text=True,
+                        default_element_size=(40, 1)).Layout(layout)
+main_window.SetIcon(r"C:\Users\Ramakant\OneDrive - University of Leicester\Financial Services Information\Assignment 2\i.ico")  # Assigning Header and variable name to the window
 
 #Main Program
 while True: #Keep application open
 
-    main_window= sg.Window('Financial Information Systems - Assignment 2', auto_size_text=True,
-                              default_element_size=(40, 1)).Layout(layout)    # Assigning Header and variable name to the window
+    values = main_window.Read()  # Reading user input data from GUI text boxes
 
-    event, button, values = main_window.Read()  # Reading user input data from GUI text boxes
-
-    #print(event,values)    #for debugging purpose
-    if event is None or event == 'Cancel': #No input / Cancel -> Quit
+    if values[0] is 'Cancel' or values[0] is None: #No input / Cancel -> Quit
        break
 
-
-
-    main_window.Close()
     #Else perform calculations
-    user_input = values
+    user_input = values[1]
     print(user_input)
-    #Empty inputs + Input validation
-    if user_input[0] == '' or user_input[1] == '' or user_input[2] == '' or user_input[0].isalpha() or user_input[1].isalpha() or user_input[2].isalpha():
-        sg.Popup('Input Error')
-        break
-
-    if user_input[3] is None and _RADIO1_ is None:
-        sg.Popup('Please tick Radio Button')
-        break
 
     # variable initialization + data type conversion
-    principal = int(user_input[0])
-    rate = float(user_input[1])
-    duration = int(user_input[2])
-    if user_input[3] is True:   #Duration entered in years
-        duration = duration * 12
-    print(principal,rate,duration)
+
+    # Empty inputs + Input validation
+    try:
+        principal = float(user_input[0])
+        rate = float(user_input[1])
+        duration = int(user_input[2])
+        if user_input[3] is True:   #Duration entered in years
+            duration = duration * 12
+        print(principal,rate,duration)
+        input_error = 0
+
+    except: #initialize dummy values and throw error
+        principal = 0
+        rate = 1
+        duration = 1
+        input_error = 1
+        sg.Popup("Input Error!", "Please check numbers & select radio!")
+
+    #Calculation variables
     payment = []
     principal_paid = []
     interest_paid = []
@@ -54,38 +78,17 @@ while True: #Keep application open
     data = []
 
     #Main calculation
-    for i in range(0,duration): #for number of months
-        payment.append((((rate/12)*principal)*(1+(rate/12))**duration)/(((1+(rate/12))**duration)-1)) #payment calculation
-        principal_paid.append(payment[i]/((1+(rate/12))**(1+duration-(i+1)))) #principal calculation
-        interest_paid.append(payment[i]-principal_paid[i]) #interest calculation
-        loan_balance.append((interest_paid[i]/(rate/12))-principal_paid[i]) #loan balance calculation
-        #data.append(str(i)+','+str(payment[i])+','+str(principal_paid[i])+','+str(interest_paid[i])+','+str(loan_balance[i]))
-        list=[(i+1),round(payment[i],2),round(principal_paid[i],2),round(interest_paid[i],2),round(loan_balance[i],2)] #To get list of each rows
-        data.append(list) #appending rows in to single list
-    #print(data)    #enabled for deubgging
+    try: #Errors while calculations
+        for i in range(0,duration): #for number of months
+            payment.append((((rate/12)*principal)*(1+(rate/12))**duration)/(((1+(rate/12))**duration)-1)) #payment calculation
+            principal_paid.append(payment[i]/((1+(rate/12))**(1+duration-(i+1)))) #principal calculation
+            interest_paid.append(payment[i]-principal_paid[i]) #interest calculation
+            loan_balance.append((interest_paid[i]/(rate/12))-principal_paid[i]) #loan balance calculation
+            list=[(i+1),round(payment[i],2),round(principal_paid[i],2),round(interest_paid[i],2),round(loan_balance[i],2)] #To get list of each rows
+            data.append(list) #appending rows in to single list
+    except: #Numbers too big?
+        input_error = 1
+        sg.Popup("Input Error! Numbers out of range")
 
-
-    # function to output table
-    def table_function(a):
-        header_list = ['Payment No', 'Payment Amount', 'Principal Paid', 'Interest Paid', 'Loan balance']   #headers
-        data_1 = a   #data is passed as parameter
-
-        # table decoration
-        sg.SetOptions(element_padding=(0, 0))
-        layout = [[sg.Table(values=data_1,
-                 headings=header_list,
-                 max_col_width=25,
-                 auto_size_columns=True,
-                 justification='center',
-                 alternating_row_color='lightblue',
-                 num_rows=min(len(data_1),20))],
-                  [sg.Exit()]]
-
-       # show output
-        window = sg.Window('Table', grab_anywhere=False).Layout(layout)
-        event, values = window.Read()
-        if event is None or event == 'Exit':
-            window.Close() #Close table window on exit
-        window.Close()
-    table_function(data)  #calling function
-    window.FindElement('_RADIO1_').Update(fahrenheit)
+    if input_error == 0:
+        table_function(data)  # calling function
